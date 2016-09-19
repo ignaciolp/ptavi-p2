4dc6abdc5cac2d172e348c3dee3d200822a8a17e
Ignacio Lopez Penalver <nacholp@gamma11.aulas.gsyc.urjc.es>
2016-09-19 10:37:44 +0200

    hecho en clase divi y multi

--- calc.py
+++ calc.py
@@ -12,6 +12,15 @@ def plus(op1, op2):
 def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
+    
+def multi(op1, op2):
+    """funcion de multiplicacion"""
+    return op1 * op2
+    
+def divi(op1,op2):
+    """funcion de dividir"""
+    return op1 / op2
+        
 
 if __name__ == "__main__":
     try:
@@ -24,6 +33,11 @@ if __name__ == "__main__":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
+    elif sys.argv[2] == "multiplicar":
+        result = multi(operando1, operando2)
+     elif sys.argv[2]== "dividir":
+        result = divi(operando1, operando2)  
+            
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
