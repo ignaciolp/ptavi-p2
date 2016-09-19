acdabe140854b1b3b60a186bdd5b0989af8aa00d
Ignacio Lopez Penalver <nacholp@alpha07.aulas.gsyc.urjc.es>
2016-09-19 13:07:36 +0200

    practica 2.4

---
+++ calcoo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+
+	def sumas(self,op1,op2):
+		return op1+op2
+
+	def restas(self,op1,op2):
+		return op1-op2
+
+if __name__=="__main__":
+	try:
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])
+		calculadora = Calculadora()
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
+	
+         
+
+	if sys.argv[2] == "suma":
+		result = calculadora.sumas(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		result = calculadora.restas(operando1, operando2)
+	else:
+		sys.exit('Operación sólo puede ser sumar o restar.')
+
+	print(result)
