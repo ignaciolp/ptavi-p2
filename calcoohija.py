6e19044b152143e97f2766f728ac07af2f7ab5cf
Ignacio Lopez Penalver <nacholp@gamma16.aulas.gsyc.urjc.es>
2016-09-19 15:17:18 +0200

    practica 2 punto 5

---
+++ calcoohija.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+
+	def sumas(self,op1,op2):
+		return op1+op2
+
+	def restas(self,op1,op2):
+		return op1-op2
+
+class Calculadorahija(Calculadora):
+	
+	def multiplicar(self,op1,op2):
+		return op1*op2
+
+	def dividir(self,op1,op2):
+		if op2 == 0:
+			return "Division by zero is not allowed"
+		else:
+			op1/op2
+		 
+
+if __name__=="__main__":
+	try:
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])
+		calculadora = Calculadora()
+		calculadorahija = Calculadorahija()
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
+	
+         
+
+	if sys.argv[2] == "suma":
+		result = calculadora.sumas(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		result = calculadora.restas(operando1, operando2)
+	elif sys.argv[2] == "multiplicar":
+		result = calculadorahija.multiplicar(operando1, operando2)
+	elif sys.argv[2] == "dividir":
+		result = calculadorahija.dividir(operando1, operando2)
+	else:
+		sys.exit('Operación sólo puede ser sumar o restar.')
+
+	print(result)
