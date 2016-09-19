f005f7fa4e0e7de7267784acacaa11721f17ce66
Ignacio Lopez Penalver <nacholp@gamma16.aulas.gsyc.urjc.es>
2016-09-19 15:40:40 +0200

    retocado

--- calcoohija.py
+++ calcoohija.py
@@ -2,21 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import calcoo
 
-class Calculadora():
-
-	def sumas(self,op1,op2):
-		return op1+op2
-
-	def restas(self,op1,op2):
-		return op1-op2
-
-class Calculadorahija(Calculadora):
+class Calculadorahija(calcoo.Calculadora):
 	
-	def multiplicar(self,op1,op2):
+	def multiplicar(self, op1, op2):
 		return op1*op2
 
-	def dividir(self,op1,op2):
+	def dividir(self, op1, op2):
 		if op2 == 0:
 			return "Division by zero is not allowed"
 		else:
@@ -26,22 +19,22 @@ class Calculadorahija(Calculadora):
 if __name__=="__main__":
 	try:
 		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-		calculadora = Calculadora()
-		calculadorahija = Calculadorahija()
+		operandor2 = str(sys.argv[2])
+		operando3 = int(sys.argv[3])
+		
 	except ValueError:
 		sys.exit("Error: Non numerical parameters")
-	
-         
 
-	if sys.argv[2] == "suma":
-		result = calculadora.sumas(operando1, operando2)
-	elif sys.argv[2] == "resta":
-		result = calculadora.restas(operando1, operando2)
-	elif sys.argv[2] == "multiplicar":
-		result = calculadorahija.multiplicar(operando1, operando2)
-	elif sys.argv[2] == "dividir":
-		result = calculadorahija.dividir(operando1, operando2)
+	calculadorahija = Calculadorahija()
+       
+	if operandor2 == "suma":
+		result = calculadorahija.sumas(operando1, operando3)
+	elif operandor2 == "resta":
+		result = calculadorahija.restas(operando1, operando3)
+	elif operandor2 == "multiplicar":
+		result = calculadorahija.multiplicar(operando1, operando3)
+	elif operandor2 == "dividir":
+		result = calculadorahija.dividir(operando1, operando3)
 	else:
 		sys.exit('Operación sólo puede ser sumar o restar.')
 
